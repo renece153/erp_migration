df5008d2ca3c0e4447410252ad6e1b38bad40663
renece153 <87631856+renece153@users.noreply.github.com>
2025-10-15 16:39:25 -0700

    Update dm_get_key_vault_creds.py

--- ETL Scripts/dm_get_key_vault_creds.py
+++ ETL Scripts/dm_get_key_vault_creds.py
@@ -3,10 +3,10 @@ from azure.keyvault.secrets import SecretClient
 
 
 def get_key_vault_secrets(_secret_name):
-    tenant_id = "91aaadf7-17fe-4e27-8046-10d35b53e9eb"
-    client_id = "d993c5aa-27a6-44ac-9728-32e944aab947"
-    client_secret = "wN68Q~o4jvAxdS3nKwHehT~Eihe_sPeq_U5QMctv"
-    vault_url = "https://erpteam-keyvault.vault.azure.net/"
+    tenant_id = "Redacted"
+    client_id = "Redacted"
+    client_secret = "Redacted"
+    vault_url = "Redacted"
     credential = ClientSecretCredential(tenant_id, client_id, client_secret)
     client = SecretClient(vault_url=vault_url, credential=credential)
     secret = client.get_secret(_secret_name)
@@ -21,4 +21,5 @@ def get_nested_value(data, path):
         else:
             return None
     return data
+
 
