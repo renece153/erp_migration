b04f2bb97d1fdf16a5d0993325f533e5727b78bc
renece153 <87631856+renece153@users.noreply.github.com>
2025-10-15 16:25:14 -0700

    Update mail_gun_email.py

--- mail_gun_email.py
+++ mail_gun_email.py
@@ -3,11 +3,12 @@ import json
 
 def send_message(_recipients):
 	return r.post(
-		"https://api.mailgun.net/v3/karddatamigration.online/messages",
-		auth=("api", "b0400a5899166a36e0b87f9112b39fcc-a26b1841-a6f9d676"),
+		"Redacted" ##Removed as its the URL of Mail Gun,
+		auth=("api", "Redacted" ## Remove as this is the API Key),
 		data={"from": "Data Migration Support <postmaster@karddatamigration.online>",
 			"to": "Support <postmaster@karddatamigration.online>",
 			"bcc": _recipients,
 			"subject": "HR Deletion Complete",
 			"template": "hr deletion complete",
+
 			"h:X-Mailgun-Variables":  json.dumps({"test": ''})})
